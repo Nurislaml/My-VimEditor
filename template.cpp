f87073dd165fe3e4f3f234facd57f63cf743af84
Mukhash Nurislam <128770869+Nurislaml@users.noreply.github.com>
2025-10-03 17:58:58 +0500

    main.cpp

--- template.cpp
+++ template.cpp
@@ -3,32 +3,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-void dbg_out() { cerr << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define int long long
+#define all(x) (x).begin(), (x).end()
 
 #ifdef LOCAL
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+template<typename... T> void dbg(T... args) { ((cerr << args << " "), ...); cerr << "\n"; }
 #else
 #define dbg(...)
 #endif
 
-#define int long long
-#define double long double
-#define all(a) (a).begin(), (a).end()
-
 void solve() {
-
+    ///    
 }
 
 int32_t main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int tc = 1;
     // cin >> tc;
-    for (int t = 1; t <= tc; t++) {
-        // cout << "Case #" << t << ": ";
-        solve();
-    }
+    while (tc--) solve();
 }
+
