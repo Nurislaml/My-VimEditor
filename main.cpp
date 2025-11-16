447a0841d348c0c621f65b63580e5dc59978cb15
Mukhash Nurislam <128770869+Nurislaml@users.noreply.github.com>
2025-11-16 19:33:53 +0500

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+#define int long long
+#define all(t) (t).begin(), (t).end()
+#define vi vector<int>
+#define pi pair<int, int>
+
+void mukh4w() { 	
+
+}
+ 
+int32_t main() {
+ 	ios::sync_with_stdio(false);
+ 	cin.tie(nullptr);
+ 	
+	int tc = 1;
+	// cin >> tc;
+ 	while (tc--) mukh4w();
+}
